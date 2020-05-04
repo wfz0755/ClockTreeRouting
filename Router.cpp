62297d4710d588136e6ee1f0ca522e59e8b25cea
hachiman hikigaya <wfz0755@hotmail.com>
2020-05-04 09:41:15 +0800

    code formatting

---
+++ Router.cpp
@@ -0,0 +1,24 @@
+#include "Router.h"
+
+void Router::init(){
+    //read input and build data structure
+}
+void Router::readInput(){
+
+}
+// Recursive Geometric Matching
+void Router::RGM(){
+
+}  
+// Deferred-Merge Embedding
+void Router::DME(){
+
+}
+
+void Router::route(){
+
+}
+
+void Router::writeSolution(){
+    
+}
--- Router.h
+++ Router.h
@@ -1,23 +1,19 @@
 #pragma once
 #include "global.h"
 using namespace std;
-class Router{
-    public:
-    
+class Router {
+public:
     string input_file_name;
     string output_file_name;
-    
 
-
-    Router(string in_name, string out_name):input_file_name(in_name),output_file_name(out_name){}
+    Router(string in_name, string out_name) : input_file_name(in_name), output_file_name(out_name) {}
 
     void init();
     void readInput();
-    void RGM();//Recursive Geometric Matching
-    void DME(); // Deferred-Merge Embedding
-    
+    void RGM();  // Recursive Geometric Matching
+    void DME();  // Deferred-Merge Embedding
+
     void route();
 
     void writeSolution();
-
 };
